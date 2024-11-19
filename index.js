e062241fcbf4423d58276f34139a1f67af6fd323
Pitt <pittrawan@gmail.com>
2024-11-18 17:47:35 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -43,3 +43,20 @@ function shuffleTruth() {
         image.setAttribute ("src", finalTruth);
     }, animationDuration);
 }
+
+function shuffleDare() {
+    const image = document.querySelector("img");
+    const animationDuration = 700;
+    const intervalSpeed = 20;
+
+    let interval = setInterval (() => {
+        const randomDare = "./images/dare" + (Math.floor(Math.random() * 10)+1) + ".png";
+        image.setAttribute ("src", randomDare);
+    }, intervalSpeed);
+
+    setTimeout (() => {
+        clearInterval(interval);
+        const finalDare = "./images/dare" + (Math.floor(Math.random() * 10)+1) + ".png";
+        image.setAttribute ("src", finalDare);
+    }, animationDuration);
+}
